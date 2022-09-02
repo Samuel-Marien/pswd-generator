da7423b1b36d1f527cab84e13164b7c4318a97b9
Samuel <timeuss.sam@gmail.com>
2022-09-02 18:06:22 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ReactSlider from 'react-slider'
+
+// const number = '0123456789'
+// const lower = 'abcdefghijklmnopqrstuvwxyz'
+// const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+// const sign = '!@#$%^&*()?'
+const chars =
+  '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()?ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 function App() {
+  const [pswd, setPswd] = useState('')
+  const [value, setValue] = useState(1)
+
+  const genPassword = (userValue) => {
+    let password = ''
+    for (var i = 0; i <= userValue; i++) {
+      let randomNumber = Math.floor(Math.random() * chars.length)
+      password += chars.substring(randomNumber, randomNumber + 1)
+    }
+    setPswd(password)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="text-center text-3xl mt-24">
+      <div>
+        <ReactSlider
+          step={1}
+          min={1}
+          max={20}
+          className="w-full h-3 pr-2 my-4 bg-gray-200 rounded-md cursor-grab"
+          thumbClassName="absolute w-5 h-5 cursor-grab bg-indigo-500 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 -top-2px"
+          value={value}
+          onChange={(value) => {
+            setValue(value)
+          }}
+        />
+        <span>{value}</span>
+
+        <button
+          onClick={() => genPassword(value - 1)}
+          className="px-2 p-y1 rounded border shadow"
         >
-          Learn React
-        </a>
-      </header>
+          click to generate
+        </button>
+      </div>
+      <p className="text-red-500 font-mono font-black text-3xl">{pswd}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
